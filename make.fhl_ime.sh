8aea92162ab3c1dda638b0cead254f45ac5dd988
李嘉育 <wox080xow@lijiayus-MacBook-Pro.local>
2023-07-08 14:05:41 +0800

    Siu-kái kha-pún.

--- make.fhl_ime.sh
+++ make.fhl_ime.sh
@@ -4,6 +4,7 @@ foot="foot.fhl_ime.txt"
 chit="1.txt"
 oo="oo.txt"
 kithann="kî-thaⁿ.txt"
+hunlui7="hun-lūi-07.txt"
 
 function catinto(){
     cat $1 >> $filename
@@ -16,4 +17,5 @@ catinto $head
 catinto $chit
 catinto $oo
 catinto $kithann
+catinto $hunlui7
 catinto $foot
