2914cd99028d0d73c3217d566be9a3288e717477
Lí Ka-io̍k <wox080xow@Li-Ka-iok-e-MacBook-Pro.local>
2023-11-24 02:33:19 +0800

    Ūi-tio̍h sin ê jī-sû-phō͘ thêng-sek hoan-sin

--- make.fhl_ime.sh
+++ make.fhl_ime.sh
@@ -5,6 +5,9 @@ chit="1.txt"
 oo="oo.txt"
 kithann="kî-thaⁿ.txt"
 hunlui7="hun-lūi-07.txt"
+kip="kip.txt"
+khkhchh="khiuⁿ-kháu-chha.txt"
+huho="hû-hō.txt"
 
 function catinto(){
     cat $1 >> $filename
@@ -18,4 +21,7 @@ catinto $chit
 catinto $oo
 catinto $kithann
 catinto $hunlui7
+catinto $kip
+catinto $khkhchh
+catinto $huho
 catinto $foot
